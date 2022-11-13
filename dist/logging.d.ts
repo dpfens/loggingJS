973e5baf3925da0d5429888ae79fca2075c2e397
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2022-11-13 03:03:50 -0500

    include dist files

---
+++ dist/logging.d.ts
@@ -0,0 +1,89 @@
+interface DataCollector {
+    collect(): any;
+}
+interface LogEntryMetadata {
+    timestamp: Date;
+    type: string;
+    data: any;
+}
+interface BaseLogEntry {
+    type: string;
+    metadata: LogEntryMetadata;
+}
+interface LogEntry extends BaseLogEntry {
+    arguments: Array<any>;
+}
+interface LogEventEntry extends BaseLogEntry {
+    event: any;
+}
+interface EntryHandler {
+    handle(entry: BaseLogEntry): boolean;
+}
+interface EventLogger {
+    handle(event: Event): boolean;
+}
+declare namespace Logging {
+    namespace collector {
+        class NavigationCollector implements DataCollector {
+            collect(): any;
+        }
+        class ScreenCollector implements DataCollector {
+            collect(): any;
+            collectScreen(): any;
+        }
+        class PerformanceCollector implements DataCollector {
+            collect(): any;
+        }
+    }
+}
+declare namespace Logging {
+    namespace handler {
+        interface EntryHandler {
+            handle(entry: BaseLogEntry): boolean;
+        }
+        export class RESTHandler implements EntryHandler {
+            protected readonly endpoint: string;
+            protected readonly method: string;
+            constructor(endpoint: string, method: string);
+            static isSupported(): boolean;
+            handleFetch(entry: BaseLogEntry): Promise<any>;
+            handleXMLHTTPRequest(entry: BaseLogEntry): void;
+            handle(entry: BaseLogEntry): boolean;
+        }
+        export {};
+    }
+}
+declare namespace Logging {
+    namespace logger {
+        class BaseLogger {
+            protected readonly collectors: Record<string, DataCollector>;
+            protected readonly handlers: Array<EntryHandler>;
+            constructor(options?: any);
+            protected toArray(iterable: any): Array<any>;
+            protected collect(): any;
+            protected gatherMetadata(): LogEntryMetadata;
+            protected executeHandlers(entry: BaseLogEntry): boolean;
+        }
+        class Logger extends BaseLogger {
+            private readonly entries;
+            private readonly groups;
+            constructor(options?: any);
+            truncateLog(): void;
+            assert(): void;
+            clear(): void;
+            debug(): void;
+            error(): void;
+            info(): void;
+            log(): void;
+            warn(): void;
+            group(): void;
+            groupEnd(): void;
+        }
+        class EventLogger extends BaseLogger implements EventLogger {
+            constructor(options?: any);
+            stringifyEvent(event: Event): string;
+            handle(event: Event): boolean;
+        }
+    }
+}
+//# sourceMappingURL=logging.d.ts.map
---
+++ dist/logging.js
@@ -0,0 +1,337 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+;
+var Logging;
+(function (Logging) {
+    var collector;
+    (function (collector) {
+        var NavigationCollector = (function () {
+            function NavigationCollector() {
+            }
+            NavigationCollector.prototype.collect = function () {
+                var data = {
+                    appCodeName: navigator.appCodeName,
+                    appName: navigator.appName,
+                    appVersion: navigator.appVersion,
+                    cookieEnabled: navigator.cookieEnabled,
+                    deviceConcurrency: navigator.deviceConcurrency,
+                    doNotTrack: navigator.doNotTrack,
+                    language: navigator.language,
+                    maxTouchPoints: navigator.maxTouchPoints,
+                    onLine: navigator.onLine,
+                    pdfViewerEnabled: navigator.pdfViewerEnabled,
+                    platform: navigator.platform,
+                    product: navigator.product,
+                    productSub: navigator.productSub,
+                    vendor: navigator.vendor,
+                    vendorSub: navigator.vendorSub,
+                    userAgent: {
+                        value: navigator.userAgent,
+                        data: JSON.parse(JSON.stringify(navigator.userAgentData))
+                    }
+                };
+                var userActivation = null;
+                if (navigator.userActivation) {
+                    userActivation = {
+                        isActive: navigator.userActivation.isActive,
+                        hasBeenActive: navigator.userActivation.hasBeenActive
+                    };
+                }
+                data.userActivation = userActivation;
+                return data;
+            };
+            return NavigationCollector;
+        }());
+        collector.NavigationCollector = NavigationCollector;
+        var ScreenCollector = (function () {
+            function ScreenCollector() {
+            }
+            ScreenCollector.prototype.collect = function () {
+                return {
+                    devicePixelRatio: window.devicePixelRatio,
+                    innerHeight: window.innerHeight,
+                    innerWidth: window.innerWidth,
+                    outerHeight: window.outerHeight,
+                    outerWidth: window.outerWidth,
+                    pageXOffset: window.pageXOffset,
+                    pageYOffset: window.pageYOffset,
+                    screen: this.collectScreen(),
+                    screenLeft: window.screenLeft,
+                    screenTop: window.screenTop,
+                    screenX: window.screenX,
+                    screenY: window.screenY,
+                    scrollX: window.scrollX,
+                    scrollY: window.scrollY
+                };
+            };
+            ScreenCollector.prototype.collectScreen = function () {
+                return {
+                    availHeight: window.screen.availHeight,
+                    availLeft: window.screen.availLeft,
+                    availTop: window.screen.availTop,
+                    availWidth: window.screen.availWidth,
+                    colorDepth: window.screen.colorDepth,
+                    height: window.screen.height,
+                    isExtended: window.screen.isExtended,
+                    orientation: {
+                        angle: window.screen.orientation.angle,
+                        type: window.screen.orientation.type
+                    },
+                    pixelDepth: window.screen.pixelDepth,
+                    width: window.screen.width
+                };
+            };
+            return ScreenCollector;
+        }());
+        collector.ScreenCollector = ScreenCollector;
+        var PerformanceCollector = (function () {
+            function PerformanceCollector() {
+            }
+            PerformanceCollector.prototype.collect = function () {
+                var output = JSON.parse(JSON.stringify(window.performance));
+                output.entries = JSON.parse(JSON.stringify(window.performance.getEntries()));
+                return output;
+            };
+            return PerformanceCollector;
+        }());
+        collector.PerformanceCollector = PerformanceCollector;
+    })(collector = Logging.collector || (Logging.collector = {}));
+})(Logging || (Logging = {}));
+var Logging;
+(function (Logging) {
+    var handler;
+    (function (handler) {
+        var RESTHandler = (function () {
+            function RESTHandler(endpoint, method) {
+                this.endpoint = endpoint;
+                this.method = method;
+            }
+            RESTHandler.isSupported = function () {
+                return true;
+            };
+            RESTHandler.prototype.handleFetch = function (entry) {
+                return fetch(this.endpoint, {
+                    method: this.method,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(entry)
+                });
+            };
+            RESTHandler.prototype.handleXMLHTTPRequest = function (entry) {
+                var req = new XMLHttpRequest();
+                req.open(this.method, this.endpoint);
+                req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                req.send(JSON.stringify(entry));
+            };
+            RESTHandler.prototype.handle = function (entry) {
+                if ('fetch' in window) {
+                    this.handleFetch(entry);
+                }
+                else {
+                    this.handleXMLHTTPRequest(entry);
+                }
+                return true;
+            };
+            return RESTHandler;
+        }());
+        handler.RESTHandler = RESTHandler;
+    })(handler = Logging.handler || (Logging.handler = {}));
+})(Logging || (Logging = {}));
+var Logging;
+(function (Logging) {
+    var logger;
+    (function (logger) {
+        var BaseLogger = (function () {
+            function BaseLogger(options) {
+                options = options || {};
+                this.collectors = options.collectors || {
+                    'navigation': new Logging.collector.NavigationCollector(),
+                    'screen': new Logging.collector.ScreenCollector(),
+                    'performance': new Logging.collector.PerformanceCollector()
+                };
+                this.handlers = options.handlers || [];
+            }
+            BaseLogger.prototype.toArray = function (iterable) {
+                var output = [];
+                for (var i = 0; i < iterable.length; i++) {
+                    output.push(iterable[i]);
+                }
+                return output;
+            };
+            BaseLogger.prototype.collect = function () {
+                var data = {};
+                for (var key in this.collectors) {
+                    var collector = this.collectors[key];
+                    data[key] = collector.collect();
+                }
+                return data;
+            };
+            BaseLogger.prototype.gatherMetadata = function () {
+                return {
+                    timestamp: new Date,
+                    data: this.collect(),
+                    type: 'Browser'
+                };
+            };
+            BaseLogger.prototype.executeHandlers = function (entry) {
+                var success = true;
+                for (var key in this.handlers) {
+                    var handler = this.handlers[key];
+                    try {
+                        handler.handle(entry);
+                    }
+                    catch (e) {
+                        console.error(e);
+                        success = false;
+                    }
+                }
+                return success;
+            };
+            return BaseLogger;
+        }());
+        logger.BaseLogger = BaseLogger;
+        var Logger = (function (_super) {
+            __extends(Logger, _super);
+            function Logger(options) {
+                var _this = _super.call(this, options) || this;
+                _this.entries = [];
+                _this.groups = [];
+                return _this;
+            }
+            Logger.prototype.truncateLog = function () {
+                this.entries.splice(0, this.entries.length);
+            };
+            Logger.prototype.assert = function () {
+                var entry = {
+                    type: 'ASSERT',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+            };
+            Logger.prototype.clear = function () {
+                var entry = {
+                    type: 'CLEAR',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+            };
+            Logger.prototype.debug = function () {
+                var entry = {
+                    type: 'DEBUG',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+            };
+            Logger.prototype.error = function () {
+                var entry = {
+                    type: 'ERROR',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+            };
+            Logger.prototype.info = function () {
+                var entry = {
+                    type: 'INFO',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+            };
+            Logger.prototype.log = function () {
+                var entry = {
+                    type: 'LOG',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+            };
+            Logger.prototype.warn = function () {
+                var entry = {
+                    type: 'WARN',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+            };
+            Logger.prototype.group = function () {
+                var entry = {
+                    type: 'GROUP',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+                if (arguments.length > 0) {
+                    var groupLabel = arguments[0];
+                    this.groups.push(groupLabel);
+                }
+            };
+            Logger.prototype.groupEnd = function () {
+                var entry = {
+                    type: 'GROUPEND',
+                    arguments: this.toArray(arguments),
+                    metadata: this.gatherMetadata()
+                };
+                this.entries.push(entry);
+                this.groups.pop();
+            };
+            return Logger;
+        }(BaseLogger));
+        logger.Logger = Logger;
+        var EventLogger = (function (_super) {
+            __extends(EventLogger, _super);
+            function EventLogger(options) {
+                var _this = _super.call(this, options) || this;
+                _this.handle = _this.handle.bind(_this);
+                return _this;
+            }
+            EventLogger.prototype.stringifyEvent = function (event) {
+                var obj = {};
+                for (var k in event) {
+                    obj[k] = event[k];
+                }
+                return JSON.stringify(obj, function (key, value) {
+                    if (value instanceof Node)
+                        return 'Node';
+                    if (value instanceof Window)
+                        return 'Window';
+                    if (value instanceof Error) {
+                        return { message: value.message, stack: value.stack };
+                    }
+                    ;
+                    return value;
+                }, ' ');
+            };
+            EventLogger.prototype.handle = function (event) {
+                var eventData = JSON.parse(this.stringifyEvent(event)), entry = {
+                    type: event.type,
+                    metadata: this.gatherMetadata(),
+                    event: eventData
+                };
+                var success = this.executeHandlers(entry);
+                return success;
+            };
+            return EventLogger;
+        }(BaseLogger));
+        logger.EventLogger = EventLogger;
+    })(logger = Logging.logger || (Logging.logger = {}));
+})(Logging || (Logging = {}));
+//# sourceMappingURL=logging.js.map
